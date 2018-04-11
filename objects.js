a02f2503639efe26c5837ba128d778efa2ad337b
Ledio Bode <l.bode515@gmail.com>
2018-04-11 23:04:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,8 @@
 var playlist = new Object ({ artist : "song" });
 
-function updatePlaylist(playlist, artist, song){
-  playlist[artist] = "song";
+function updatePlaylist(playlist, artist, song) {
+  
+  playlist[artist] = 'song';
   return playlist;
+  
 }
